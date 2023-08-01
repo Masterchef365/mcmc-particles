87fc56ac4b3f742e6eaa308f819dd050e56950af
Masterchef365 <duncan.freeman1@gmail.com>
2023-08-01 10:23:30 -0700

    Very basic mcmc approach, slow

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ impl UserState for ClientState {
 
         sched.add_system(Self::update_sim).build();
 
-        let sim = Sim::new(1_000);
+        let sim = Sim::new(100);
 
         Self { ui, sim }
     }
@@ -41,55 +41,78 @@ impl UserState for ClientState {
 
 impl ClientState {
     fn update_ui(&mut self, io: &mut EngineIo, _query: &mut QueryResult) {
+        let energy = total_energy(&self.sim.current);
+
+        //cimvr_engine_interface::println!("{}", energy);
+
         self.ui.show(io, |ui| {
-            if ui.button("Fuck yeah").clicked() {
-                cimvr_engine_interface::println!("I love egui!");
-            }
+            ui.label(format!("Total energy: {energy:.003}"));
         });
     }
 
     fn update_sim(&mut self, io: &mut EngineIo, _query: &mut QueryResult) {
         self.sim.step();
         io.send(&UploadMesh {
-            mesh: particles_mesh(&self.sim),
+            mesh: state_mesh(&self.sim.current),
             id: PARTICLES_RDR,
         });
     }
 }
 
-fn particles_mesh(sim: &Sim) -> Mesh {
-    let vertices = sim
+fn state_mesh(state: &State) -> Mesh {
+    let vertices = state
         .positions
         .iter()
         .map(|p| Vertex::new([p.x, 0., p.y], [1.; 3]))
         .collect();
-    let indices = (0..sim.positions.len() as u32).collect();
+    let indices = (0..state.positions.len() as u32).collect();
     Mesh { vertices, indices }
 }
 
-struct Sim {
+#[derive(Clone)]
+struct State {
     positions: Vec<Vec2>,
+}
+
+struct Sim {
+    current: State,
 }
 
 impl Sim {
     pub fn new(n: usize) -> Self {
         let mut rng = rng();
 
+        let s = 0.1;
+
         let positions = (0..n)
-            .map(|_| Vec2::new(rng.gen_range(-1.0..=1.0), rng.gen_range(-1.0..=1.0)))
+            .map(|_| Vec2::new(rng.gen_range(-s..=s), rng.gen_range(-s..=s)))
             .collect();
 
-        Self { positions }
+        let current = State { positions };
+
+        Self { current }
     }
 
     pub fn step(&mut self) {
         let ref mut rng = rng();
         let normal = Normal::new(0.0, 0.001).unwrap();
 
-        for _ in 0..self.positions.len() {
-            let part = self.positions.choose_mut(rng).unwrap();
+        let n_steps = 100;
+
+
+        for _ in 0..n_steps {
+            let mut new_state = self.current.clone();
+
+            let part = new_state.positions.choose_mut(rng).unwrap();
             part.x += normal.sample(rng);
             part.y += normal.sample(rng);
+
+            let old_energy = total_energy(&self.current);
+            let new_energy = total_energy(&new_state);
+
+            if new_energy < old_energy {
+                self.current = new_state;
+            }
         }
     }
 }
@@ -97,4 +120,23 @@ impl Sim {
 fn rng() -> SmallRng {
     let u = ((Pcg::new().gen_u32() as u64) << 32) | Pcg::new().gen_u32() as u64;
     SmallRng::seed_from_u64(u)
+}
+
+fn potential(a: Vec2, b: Vec2) -> f32 {
+    let r = a.distance(b);
+    let epsilon = 0.01;
+    let attract = 0.01;
+    let repulse = 0.01;
+
+    4. * epsilon * ((repulse / r).powi(12) - (attract / r).powi(6))
+}
+
+fn total_energy(state: &State) -> f32 {
+    let mut sum = 0.;
+    for i in 0..state.positions.len() {
+        for j in (i + 1)..state.positions.len() {
+            sum += potential(state.positions[i], state.positions[j]);
+        }
+    }
+    sum
 }

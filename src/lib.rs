a70c0180829bf1eaf1cf7b75070bbb09a67e0076
Masterchef365 <duncan.freeman1@gmail.com>
2023-08-01 22:54:29 -0700

    Particle life rules

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,7 @@ impl UserState for ClientState {
 
         let n = 500;
 
-        let sim = Sim::new(n, potential_cutoff, editor_potential, 10.0, 0.0003);
+        let sim = Sim::new(n, potential_cutoff, Ruleset::default(), 10.0, 0.0003);
 
         Self {
             ui,
@@ -119,7 +119,7 @@ impl ClientState {
                     self.sim = Sim::new(
                         self.n,
                         self.potential_cutoff,
-                        self.editor_potential,
+                        Ruleset::default(),
                         self.sim.temperature,
                         self.sim.walk_sigma,
                     );
@@ -134,18 +134,20 @@ impl ClientState {
         }
 
         io.send(&UploadMesh {
-            mesh: state_mesh(&self.sim.state),
+            mesh: state_mesh(&self.sim.state, &self.sim.rules),
             id: PARTICLES_RDR,
         });
     }
 }
 
-fn state_mesh(state: &State) -> Mesh {
+fn state_mesh(state: &State, rules: &Ruleset) -> Mesh {
     let vertices = state
         .positions
         .iter()
-        .map(|p| Vertex::new([p.x, 0., p.y], [1.; 3]))
+        .zip(&state.types)
+        .map(|(pos, ty)| Vertex::new([pos.x, 0., pos.y], rules.particles[*ty as usize].color))
         .collect();
+
     let indices = (0..state.positions.len() as u32).collect();
     Mesh { vertices, indices }
 }
@@ -161,24 +163,19 @@ struct LennardJones {
 #[derive(Clone)]
 struct State {
     positions: Vec<Vec2>,
+    types: Vec<u8>,
 }
 
 struct Sim {
     state: State,
-    potential: LennardJones,
     accel: QueryAccelerator,
+    rules: Ruleset,
     temperature: f32,
     walk_sigma: f32,
 }
 
 impl Sim {
-    pub fn new(
-        n: usize,
-        cutoff: f32,
-        potential: LennardJones,
-        temperature: f32,
-        walk_sigma: f32,
-    ) -> Self {
+    pub fn new(n: usize, cutoff: f32, rules: Ruleset, temperature: f32, walk_sigma: f32) -> Self {
         let mut rng = rng();
 
         let s = 0.1;
@@ -187,26 +184,45 @@ impl Sim {
             .map(|_| Vec2::new(rng.gen_range(-s..=s), rng.gen_range(-s..=s)))
             .collect();
 
-        let state = State { positions };
+        let rules = Ruleset::default();
 
-        let radius = potential.solve(cutoff);
+        let types = (0..n)
+            .map(|_| rng.gen_range(0..rules.particles.len() as u8))
+            .collect();
+
+        let state = State { positions, types };
+
+        let radius = rules
+            .particles
+            .iter()
+            .map(|rule| {
+                rule.interactions
+                    .iter()
+                    .map(|r| r.solve(cutoff))
+                    .max_by(|a, b| a.total_cmp(b))
+                    .unwrap()
+            })
+            .max_by(|a, b| a.total_cmp(b))
+            .unwrap();
+
         cimvr_engine_interface::println!("Radius: {}", radius);
 
         let accel = QueryAccelerator::new(&state.positions, radius);
 
         Self {
             state,
-            potential,
             accel,
             temperature,
             walk_sigma,
+            rules,
         }
     }
 
     pub fn set_potential(&mut self, potential: LennardJones, cutoff: f32) {
         let radius = potential.solve(cutoff);
         self.accel = QueryAccelerator::new(&self.state.positions, radius);
-        self.potential = potential;
+        //self.potential = potential;
+        todo!();
     }
 
     pub fn step(&mut self) {
@@ -237,9 +253,16 @@ impl Sim {
 
     pub fn energy_due_to(&self, idx: usize, pos: Vec2) -> f32 {
         let mut energy = 0.;
+
+        let my_ruleset = self.state.types[idx] as usize;
+        let my_ruleset = &self.rules.particles[my_ruleset];
+
         for neighbor in self.accel.query_neighbors(&self.state.positions, idx, pos) {
             let distance = self.state.positions[neighbor].distance(pos);
-            let potential = self.potential.eval(distance);
+            let potential = self.state.types[neighbor] as usize;
+            let potential = my_ruleset.interactions[potential];
+
+            let potential = potential.eval(distance);
             energy += potential;
         }
         energy
@@ -285,6 +308,50 @@ impl Default for LennardJones {
         Self {
             attract: 0.01,
             repulse: 0.007,
+        }
+    }
+}
+
+struct Ruleset {
+    particles: Vec<ParticleRules>,
+}
+
+struct ParticleRules {
+    color: [f32; 3],
+    interactions: Vec<LennardJones>,
+}
+
+impl Default for Ruleset {
+    fn default() -> Self {
+        Self {
+            particles: vec![
+                ParticleRules {
+                    color: [1., 0.1, 0.1],
+                    interactions: vec![
+                        LennardJones {
+                            attract: 0.01,
+                            repulse: 0.007,
+                        },
+                        LennardJones {
+                            attract: 0.03,
+                            repulse: 0.007,
+                        },
+                    ],
+                },
+                ParticleRules {
+                    color: [0.1, 0.5, 1.0],
+                    interactions: vec![
+                        LennardJones {
+                            attract: 0.01,
+                            repulse: 0.07,
+                        },
+                        LennardJones {
+                            attract: 0.01,
+                            repulse: 0.007,
+                        },
+                    ],
+                },
+            ],
         }
     }
 }
